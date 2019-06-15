57dd7c2fa2b41f717f7cbcdcef2bf9ffc025f28f
skin93 <arro.ciesielski@gmail.com>
2019-06-15 17:10:09 +0200

    ContactForm added

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -3,66 +3,72 @@ import uuid from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
-    ADD_CONTACT,
-    DELETE_CONTACT,
-    SET_CURRENT,
-    CLEAR_CURRENT,
-    UPDATE_CONTACT,
-    FILTER_CONTACTS,
-    CLEAR_FILTER
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
 } from '../types';
 
 const ContactState = props => {
-    const initialState = {
-        contacts: [
-            {
-                id: 1,
-                name: 'Jill Johnson',
-                email: 'jill@gmail.com',
-                phone: '111-111-111',
-                type: 'personal'
-            },
-            {
-                id: 2,
-                name: 'Sara Watson',
-                email: 'sara@gmail.com',
-                phone: '222-222-222',
-                type: 'personal'
-            },
-            {
-                id: 3,
-                name: 'Harry White',
-                email: 'harry@gmail.com',
-                phone: '333-333-333',
-                type: 'professional'
-            }
-        ]
-    };
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Johnson',
+        email: 'jill@gmail.com',
+        phone: '111-111-111',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'Sara Watson',
+        email: 'sara@gmail.com',
+        phone: '222-222-222',
+        type: 'personal'
+      },
+      {
+        id: 3,
+        name: 'Harry White',
+        email: 'harry@gmail.com',
+        phone: '333-333-333',
+        type: 'professional'
+      }
+    ]
+  };
 
-    const [state, dispatch] = useReducer(contactReducer, initialState);
+  const [state, dispatch] = useReducer(contactReducer, initialState);
 
-    // add contact
+  // add contact
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
-    // delete contact
+  // delete contact
 
-    // set current contact
+  // set current contact
 
-    // clear current contact
+  // clear current contact
 
-    // update contact
+  // update contact
 
-    // filter contacts
+  // filter contacts
 
-    //clear filter
+  //clear filter
 
-    return (
-        <ContactContext.Provider
-            value={{
-                contacts: state.contacts
-            }}>
-            {props.children}
-        </ContactContext.Provider>
-    )
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        addContact
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
 };
 
 export default ContactState;

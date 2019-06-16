d9bb04861a26981cbb248ae9186acb74845db004
skin93 <arro.ciesielski@gmail.com>
2019-06-16 12:23:44 +0200

    AuthContext added

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,50 @@
+import React, { useReducer } from 'react';
+import AuthContext from '../auth/authContext';
+import authReducer from '../auth/authReducer';
+import {
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from '../types';
+
+const AuthState = props => {
+  const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null
+  };
+
+  const [state, dispatch] = authReducer(authReducer, initialState);
+
+  // load user
+
+  // register user
+
+  // login user
+
+  // logout
+
+  // clear errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
